cd83b5bb15e3d5d964a38bfdbdbf9795b7ab4a41
Preston Peck <pppeck313@gmail.com>
2020-11-16 04:44:52 -0500

    [Bugfix] Fixed possible mismatch between input object and prune rule object by adding prune rule cursor to scope it

--- DisjunctiveUnionParser.py
+++ DisjunctiveUnionParser.py
@@ -1,23 +1,35 @@
 def parse_disjunctive_union():
-    inside_pruned_input = False
+    start_of_prune_object_rule = -1
 
     with open("schema.graphql", "r") as infile, \
             open("schema_out.graphql", "w") as outfile:
         for line in infile:
             exclude_line = False
 
-            if line == "}\n" or line == "}":
-                inside_pruned_input = False
+            if "}\n" in line \
+                    or "}" in line:
+                start_of_prune_object_rule = -1
 
             else:
+                count = 0
+
                 with open("schema_pruned.graphql", "r") as rules:
                     for rule in rules:
-                        if "{" in line and line.replace(" ", "") == rule.replace("#", "").replace(" ", ""):
-                            inside_pruned_input = True
+                        if start_of_prune_object_rule == -1 \
+                                and "{\n" in line \
+                                and line.replace(" ", "") == rule.replace("#", "").replace(" ", ""):
+                            start_of_prune_object_rule = count
                             break
 
-                        elif inside_pruned_input and line.replace(" ", "") == rule.replace("#", "").replace(" ", ""):
-                            exclude_line = True
+                        elif count >= start_of_prune_object_rule:
+                            if line.replace(" ", "") == rule.replace("#", "").replace(" ", ""):
+                                exclude_line = True
+
+                            elif "}\n" in line \
+                                    or "}" in line:
+                                break
+
+                        count += 1
 
             if not exclude_line:
                 outfile.write(line)
